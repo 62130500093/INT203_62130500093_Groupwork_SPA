6f0d83468c1c707c638c19ac01d818f7f721ac88
62130500093 <sahachai.kmutt@mail.kmutt.ac.th>
2021-04-13 17:20:47 +0700

    finish the main function add/edit/delete and little feature

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,14 +1,34 @@
 <template>
-  <div class="cart">
     <h1>Cart</h1>
+  <div class="grid grid-flow-col grid-rows-1 gap-4 border-2">
     <!-- {{itemsInCart}} -->
-    <ul class="border border-black border-10" v-for="item in itemsInCart" :key="item.id">
-        <li class="flex text-center justify-center items-center">
-            <span><img width="200" height="200" src="https://www.myskinrecipes.com/shop/1446-thickbox/banana-flavor-%E0%B8%A3%E0%B8%AA%E0%B8%81%E0%B8%A5%E0%B9%89%E0%B8%A7%E0%B8%A2.jpg" /></span>
-            <span>{{ item.name }}</span>
-            <span>&nbsp;{{ item.amount }}</span>
-        </li>
-    </ul>
+    <div
+      class="border border-black border-10"
+      v-for="item in itemsInCart"
+      :key="item.id"
+    >
+      <div class="flex flex-col text-center justify-center items-center">
+        <span
+          ><img
+            width="50"
+            height="50"
+            src="https://www.myskinrecipes.com/shop/1446-thickbox/banana-flavor-%E0%B8%A3%E0%B8%AA%E0%B8%81%E0%B8%A5%E0%B9%89%E0%B8%A7%E0%B8%A2.jpg"
+        /></span>
+        <span>{{ item.name }}</span>
+      </div>
+        <span v-if="!item.editAmountItem">&nbsp;{{ item.amount }}</span>
+        <form v-if="item.editAmountItem">
+          <span><button @click.prevent="minusAmount(item)">&lt;</button></span>
+          <input type="text" name="amountOfItem" v-model="item.amount" />
+          <span><button @click.prevent="plusAmount(item)">&gt;</button></span>
+          <button @click.prevent ="saveChange(item)">save</button>
+        </form>
+        <button v-if="!item.editAmountItem" @click="item.editAmountItem = true">
+          edit
+        </button>
+        <span>&nbsp;<button @click="deleteItem(item)">--</button></span>
+  
+    </div>
   </div>
 </template>
 
@@ -16,13 +36,40 @@
 export default {
   name: "Cart",
   props: {
-      itemsInCart : Array
+    itemsInCart: Array,
   },
-  data(){
-      return{
-          cart : this.itemsInCart
+  emits: ["delete-item","new-amount-item"],
+  data() {
+    return {
+      cart: this.itemsInCart,
+      amountOfItem: null,
+      editAmountItem: false,
+    };
+  },
+  methods: {
+    saveChange(item) {
+      if(item.amount > 50) item.amount = 50;
+      item.editAmountItem = false;
+      this.$emit('new-amount-item',item,item.amount);
+    },
+    minusAmount(item){
+      if(item.amount == 0) {item.amout = 0;}
+      else{
+        item.amount--;
       }
-  }
+    },
+    plusAmount(item){
+      if(item.amount > 50) {item.amount = 50;}
+      else{
+        item.amount++;
+      }
+    }
+    ,
+    deleteItem(item) {
+      this.$emit("delete-item", item);
+    },
+    submitForm() {},
+  },
 };
 </script>
 
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="cart">
-    <cart-item :itemsInCart="cartItem" />
+    <cart-item
+      @new-amount-item="editCartItem"
+      @delete-item="deleteCartItem"
+      :itemsInCart="cartItem"
+    />
   </div>
   <div class="product">
     <product-item @add-item-to-cart="showItemtoCart" />
@@ -29,9 +33,7 @@ export default {
       let checkItem = 0;
       this.cartItem.map((item) => (item.id == additem.id ? checkItem++ : item));
       if (checkItem > 0) {
-        this.cartItem.map((item) =>
-          item.id == additem.id ? this.editCartItem({id: item.id, name:item.name, amount:item.amount}) : item
-        );
+        console.log(additem.name + " is already in cart.");
       } else {
         this.item = { id: additem.id, name: additem.name, amount: 1 };
         this.addItemtoCart(this.item);
@@ -57,22 +59,43 @@ export default {
         console.log(`Could not save ! ${error}`);
       }
     },
-    async editCartItem(editingItem){
-    editingItem.amount++;
+    async editCartItem(editingItem, newAmount) {
+      if (newAmount > 50) {
+        console.log("maximun amount !");
+        return;
+      }
+      console.log(newAmount);
       try {
         const res = await fetch(`${this.cartUrl}/${editingItem.id}`, {
           method: "PUT",
-          headers:{
-            'content-type' : 'application/json'
+          headers: {
+            "content-type": "application/json",
           },
-          body: JSON.stringify({id: editingItem.id,name: editingItem.name, amount: editingItem.amount})
+          body: JSON.stringify({
+            id: editingItem.id,
+            name: editingItem.name,
+            amount: newAmount,
+          }),
         });
         const data = await res.json();
         console.log(data);
-        this.cartItem = this.cartItem.map(item => item.id == editingItem.id ? {...item,amount:data.amount} : item);
-    }
-    catch (error) {
+        this.cartItem = this.cartItem.map((item) =>
+          item.id == editingItem.id ? { ...item, amount: data.amount } : item
+        );
+      } catch (error) {
         console.log(`Could not edit ! ${error}`);
+      }
+    },
+    async deleteCartItem(deleteItem) {
+      try {
+        await fetch(`${this.cartUrl}/${deleteItem.id}`, {
+          method: "DELETE",
+        });
+        this.cartItem = this.cartItem.filter(
+          (item) => item.id !== deleteItem.id
+        );
+      } catch (error) {
+        console.log(`Could not save ! ${error}`);
       }
     },
     async getCartItems() {
